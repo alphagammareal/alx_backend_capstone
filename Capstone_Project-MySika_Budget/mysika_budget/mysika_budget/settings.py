5659e27c2bd8237cca39eadf04b5ba86e40576b5
alphagammareal <sakyialpha2@gmail.com>
2025-11-23 23:07:00 +0000

    apps added to settings.py and database changed to MySQL

--- Capstone_Project-MySika_Budget/mysika_budget/mysika_budget/settings.py
+++ Capstone_Project-MySika_Budget/mysika_budget/mysika_budget/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
+    'wallets',
+    'categories',
+    'transactions',
+    'investments',
+    'analytics',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +81,15 @@ WSGI_APPLICATION = 'mysika_budget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysika_budget_db',
+        'USER': 'root',
+        'PASSWORD': 'AlphaSql@2000',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
