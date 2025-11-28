07523fd0a04456698c5c45c4d42522dee752196e
Alpha_Gamma2 <wargod410@gmail.com>
2025-11-28 13:26:27 +0000

    USERS - added register, Log in and Profile views to views.py

--- Capstone_Project-MySika_Budget/mysika_budget/users/views.py
+++ Capstone_Project-MySika_Budget/mysika_budget/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegisterSerializer, UserSerializer
+from .models import User
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+# Register View
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    queryset = User.objects.all()
+
+# Profile View
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
