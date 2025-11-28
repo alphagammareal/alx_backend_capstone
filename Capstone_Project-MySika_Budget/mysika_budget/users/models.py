8fb958a0d5229f09d6bfc712d2451b67d9a975d1
Alpha_Gamma2 <wargod410@gmail.com>
2025-11-28 13:07:49 +0000

    User models added in User app and serializers

--- Capstone_Project-MySika_Budget/mysika_budget/users/models.py
+++ Capstone_Project-MySika_Budget/mysika_budget/users/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, BaseUserManager)
+from django.utils import timezone
+import uuid
 
 # Create your models here.
+
+# Custom User manager
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email field is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
+    
+# Custom User Model
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['full_name']  
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
