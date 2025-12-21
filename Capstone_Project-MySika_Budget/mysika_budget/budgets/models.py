702ec82afd3f33cb264f6a07152ccdd327ca3bcf
alphagammareal <sakyialpha2@gmail.com>
2025-12-21 10:00:10 +0000

    integrated categories to budget app

--- Capstone_Project-MySika_Budget/mysika_budget/budgets/models.py
+++ Capstone_Project-MySika_Budget/mysika_budget/budgets/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-
+from categories.models import Category
 
 User = get_user_model()
 
@@ -22,7 +22,8 @@ class Budget(models.Model):
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "budgets")
-    category = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     amount_limit = models.DecimalField(max_digits=12, decimal_places=2)
     period = models.CharField(max_length=20, choices=PERIOD_CHOICES, default="monthly")
     
