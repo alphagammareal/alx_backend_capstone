9c22a04e537c0967d486db0fb3d71629b8a772cb
ALPHA SAKYI <sakyialpha2@gmail.com>
2025-12-25 12:46:52 +0000

    urls.py

--- Capstone_Project-MySika_Budget/mysika_budget/mysika_budget/urls.py
+++ Capstone_Project-MySika_Budget/mysika_budget/mysika_budget/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.views import ProfileAPIView, LogoutAPIView
-from django.contrib.auth.views import LogoutView  # Good import
+from django.contrib.auth.views import LogoutView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # This fixes the admin logout perfectly!
+
     path('admin/logout/', LogoutView.as_view(next_page='/login/')),
 
-    # Users authentication at root
     path('', include('users.urls', namespace='users')), 
 
     # Frontend pages
@@ -19,7 +18,7 @@ urlpatterns = [
     path('api/auth/profile/', ProfileAPIView.as_view(), name='api_profile'),
     path('api/auth/logout/', LogoutAPIView.as_view(), name='api_logout'),
 
-    # Your app APIs
+    # app APIs
     path('api/budgets/', include('budgets.urls')),
     path('api/categories/', include('categories.urls')),
     path('api/transactions/', include('transactions.urls')),
